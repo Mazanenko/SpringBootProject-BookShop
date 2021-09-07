0805756581b85bb318a8e5befe6a84c2d33034f9
Mazanenko <mazanenko@inbox.ru>
2021-09-07 23:38:18 +0300

    starting first spring boot web app

---
+++ src/main/java/com/mazanenko/petproject/firstspringcrudapp/FirstSpringCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.mazanenko.petproject.firstspringcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringCrudAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mazanenko/petproject/firstspringcrudapp/FirstSpringCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazanenko.petproject.firstspringcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
