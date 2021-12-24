5a81a9f09ef0d98b9df89583e12a50285431ac30
Mazanenko <mazanenko@inbox.ru>
2021-12-25 00:01:18 +0300

    transferred DeliveryAddress entity to JPA.

--- src/main/java/com/mazanenko/petproject/bookshop/entity/DeliveryAddress.java
+++ src/main/java/com/mazanenko/petproject/bookshop/entity/DeliveryAddress.java
@@ -1,31 +1,48 @@
 package com.mazanenko.petproject.bookshop.entity;
 
 
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
+import java.util.Objects;
 
+@Entity
+@Table(name = "delivery_address")
 public class DeliveryAddress {
-    private int customerId;
+
+    @Id
+    @Column(name = "customer_id")
+    private Long id;
 
     @NotBlank(message = "Should be not empty")
+    @Column(name = "country")
     private String country;
 
     @NotBlank(message = "Should be not empty")
+    @Column(name = "city")
     private String city;
 
     @NotBlank(message = "Should be not empty")
+    @Column(name = "street")
     private String street;
 
     @Positive(message = "Should be positive")
+    @Column(name = "house_number")
     private int houseNumber;
 
+    @Column(name = "note")
     private String note;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 
     public DeliveryAddress() {}
 
-    public DeliveryAddress(int customerId, String country, String city, String street, int houseNumber,
+    public DeliveryAddress(Long id, String country, String city, String street, int houseNumber,
                            String note) {
-        this.customerId = customerId;
+        this.id = id;
         this.country = country;
         this.city = city;
         this.street = street;
@@ -33,12 +50,12 @@ public class DeliveryAddress {
         this.note = note;
     }
 
-    public int getCustomerId() {
-        return customerId;
+    public Long getId() {
+        return id;
     }
 
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getCountry() {
@@ -81,15 +98,37 @@ public class DeliveryAddress {
         this.note = note;
     }
 
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
     @Override
     public String toString() {
         return "DeliveryAddress{" +
-                "customerId=" + customerId +
                 ", country='" + country + '\'' +
                 ", city='" + city + '\'' +
                 ", street='" + street + '\'' +
                 ", houseNumber=" + houseNumber +
                 ", note='" + note + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DeliveryAddress that = (DeliveryAddress) o;
+        return houseNumber == that.houseNumber && id.equals(that.id) && Objects.equals(country, that.country)
+                && Objects.equals(city, that.city) && Objects.equals(street, that.street)
+                && Objects.equals(note, that.note);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, country, city, street, houseNumber, note);
     }
 }
