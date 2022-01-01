cbb8cc5ff5033337cdfa28447707e681119b9c36
Mazanenko <mazanenko@inbox.ru>
2022-01-01 18:40:49 +0300

    added logger to SubscriptionServiceImpl

--- src/main/java/com/mazanenko/petproject/bookshop/service/SubscriptionService.java
+++ src/main/java/com/mazanenko/petproject/bookshop/service/SubscriptionService.java
@@ -1,14 +1,8 @@
 package com.mazanenko.petproject.bookshop.service;
 
-import com.mazanenko.petproject.bookshop.entity.Subscription;
-
 public interface SubscriptionService {
 
-    void subscribe(Subscription subscription);
-
     void subscribeByCustomerEmail(Long productId, String email);
-
-    void unsubscribe(Subscription subscription);
 
     void unsubscribeByCustomerEmail(Long productId, String email);
 }
--- src/main/java/com/mazanenko/petproject/bookshop/service/impl/SubscriptionServiceImpl.java
+++ src/main/java/com/mazanenko/petproject/bookshop/service/impl/SubscriptionServiceImpl.java
@@ -8,6 +8,8 @@ import com.mazanenko.petproject.bookshop.repository.SubscriptionRepository;
 import com.mazanenko.petproject.bookshop.service.BookService;
 import com.mazanenko.petproject.bookshop.service.CustomerService;
 import com.mazanenko.petproject.bookshop.service.SubscriptionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,7 @@ public class SubscriptionServiceImpl implements SubscriptionService {
     private final CustomerService customerService;
     private final BookService bookService;
     private final ApplicationEventPublisher applicationEventPublisher;
+    private final static Logger LOGGER = LoggerFactory.getLogger(SubscriptionServiceImpl.class);
 
     @Autowired
     public SubscriptionServiceImpl(SubscriptionRepository subscriptionRepo, CustomerService customerService,
@@ -28,14 +31,6 @@ public class SubscriptionServiceImpl implements SubscriptionService {
         this.customerService = customerService;
         this.bookService = bookService;
         this.applicationEventPublisher = applicationEventPublisher;
-    }
-
-    @Override
-    public void subscribe(Subscription subscription) {
-        if (subscription == null) {
-            return;
-        }
-        subscriptionRepo.save(subscription);
     }
 
     @Override
@@ -53,14 +48,10 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 
         subscribe(new Subscription(book, customer));
         publishSubscriptionEvent("subscribed", email, productId);
-    }
 
-    @Override
-    public void unsubscribe(Subscription subscription) {
-        if (subscription == null) {
-            return;
-        }
-        subscriptionRepo.delete(subscription);
+        LOGGER.info("The customer {} {} with ID {} and email {} subscribed to new arrives of book {} by {} with ID {}",
+                customer.getName(), customer.getSurname(), customer.getId(), customer.getEmail(),
+                book.getName(), book.getAuthor(), book.getId());
     }
 
     @Override
@@ -75,22 +66,48 @@ public class SubscriptionServiceImpl implements SubscriptionService {
             return;
         }
 
-        Subscription subscription = customer.getSubscriptions().stream()
-                .filter(subscription1 -> subscription1.getBook().getId().equals(productId))
-                .findAny().orElse(null);
+        Subscription subscription = getSubscriptionByBookId(productId, customer);
         if (subscription == null) {
             return;
         }
 
         unsubscribe(subscription);
-        customer.getSubscriptions().removeIf(subscription1 -> subscription1.getBook().getId().equals(productId));
+        customer.getSubscriptions().removeIf(subscript -> subscript.getBook().getId().equals(productId));
         publishSubscriptionEvent("unsubscribed", email, productId);
+
+        LOGGER.info("The customer {} {} with ID {} and email {} unsubscribed to new arrives of book with ID {}",
+                customer.getName(), customer.getSurname(), customer.getId(), customer.getEmail(),
+                productId);
+    }
+
+
+    private void subscribe(Subscription subscription) {
+        if (subscription == null) {
+            return;
+        }
+        subscriptionRepo.save(subscription);
+    }
+
+    private void unsubscribe(Subscription subscription) {
+        if (subscription == null) {
+            return;
+        }
+        subscriptionRepo.delete(subscription);
     }
 
     private void publishSubscriptionEvent(String name, String customerEmail, Long productId) {
-        if (name == null || customerEmail == null || productId <=0) {
+        if (name == null || customerEmail == null || productId <= 0) {
             return;
         }
         applicationEventPublisher.publishEvent(new CustomerSubscriptionEvent(name, customerEmail, productId));
+    }
+
+    private Subscription getSubscriptionByBookId(Long bookId, Customer customer) {
+        if (bookId == null || customer == null) {
+            return null;
+        }
+        return customer.getSubscriptions().stream()
+                .filter(subscription1 -> subscription1.getBook().getId().equals(bookId))
+                .findAny().orElse(null);
     }
 }
